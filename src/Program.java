aec8582d086c28761f721d45b73ba248e057c3a5
newlec <newlec@namoolab.com>
2019-01-08 20:52:49 +0900

    examples for java lesson

---
+++ src/Program.java
@@ -0,0 +1,20 @@
+public class Program {
+	public static void main(String[] args) {
+
+		int kor1, kor2, kor3;
+		int total;
+		float avg;
+
+		kor1 = 50;
+		kor2 = 60;
+		kor3 = 80;
+
+		total = kor1 + kor2 + kor3;
+		avg = total / 3; 
+
+		System.out.printf("total is %d\n", total);
+		System.out.printf("avg is %f\n", avg);
+		
+
+	}
+}
